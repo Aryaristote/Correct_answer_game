444afb502f2894a63eebf396e0d7ad4d19a2f5af
Ary <aryaristote@gmail.com>
2023-03-22 11:37:25 +0200

    update

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -25,26 +25,6 @@
           <button @click="next" type="button" class="btn btn-warning">Next Queston</button>
         </div>
       </div>
-      <div class="table-responsive">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Love</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
     </div>
   </main>
 </template>
